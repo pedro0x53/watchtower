92fb9de42cf5aa248e8ba3ec614240a925f36193
Pedro Sousa <pedrosousa2708@gmail.com>
2023-04-08 20:53:22 -0300

    Login update

--- Watchtower/Watchtower/Features/Login/View/LoginView.swift
+++ Watchtower/Watchtower/Features/Login/View/LoginView.swift
@@ -9,40 +9,49 @@ import SwiftUI
 import AuthenticationServices
 
 struct LoginView: View {
-    @ObservedObject var viewModel: LoginViewModel
+    @State private var shouldShowInfo: Bool = false
 
     var body: some View {
-        ZStack {
-            BackgroundCastleView()
+        NavigationStack {
+            ZStack {
+                BackgroundCastleView()
 
-            VStack {
-                Spacer()
+                VStack {
+                    Spacer()
 
-                LogoView()
+                    LogoView()
 
-                Spacer()
+                    Spacer()
 
-                PrimaryActionButton("Iniciar Sessão Local",
-                                    action: didTaplocalSessionButton)
+                    NavigationLink {
+                        DashboardView()
+                    } label: {
+                        Text("Iniciar Sessão Local")
+                            .bold()
+                            .padding(4)
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
 
-                Spacer()
+                    Spacer()
 
-                SecondaryActionButton("O que é a Sessão Local?",
-                                      action: didTaplocalSessionInfoButton)
+                    SecondaryActionButton("O que é a Sessão Local?") {
+                        shouldShowInfo = true
+                    }
+                    .sheet(isPresented: $shouldShowInfo, content: {
+                        InfoView()
+                            .presentationDetents([.medium])
+                    })
+                }
             }
             .padding()
         }
     }
-
-    func didTaplocalSessionButton() {
-        viewModel.startLocalSession()
-    }
-
-    func didTaplocalSessionInfoButton() {}
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView(viewModel: LoginViewModel())
+        LoginView()
+            .tint(.flame)
     }
 }
